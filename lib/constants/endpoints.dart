d32c5ebd43214df20ffbf478be291b09f3dafb47
Arthur Piccoli <arthurpiccoli@outlook.com>
2020-09-16 02:13:46 -0300

    create constants

---
+++ lib/constants/endpoints.dart
@@ -0,0 +1,10 @@
+abstract class Endpoints {
+  static const String apiUrl = "https://api.themoviedb.org/3";
+  static const String backdropUrl =
+      "https://image.tmdb.org/t/p/w1000_and_h450_multi_faces";
+  static const String posterUrl =
+      "https://image.tmdb.org/t/p/w220_and_h330_face";
+
+  static const String upcomingMovies = "/movie/upcoming";
+  static const String searchMovies = "/search/movie";
+}
