7f018855ad98a57945022e017fcd6ec8843b2354
Arthur Piccoli <arthurpiccoli@outlook.com>
2020-09-17 13:57:33 -0300

    update endpoints

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -3,8 +3,9 @@ abstract class Endpoints {
   static const String backdropUrl =
       "https://image.tmdb.org/t/p/w1000_and_h450_multi_faces";
   static const String posterUrl =
-      "https://image.tmdb.org/t/p/w220_and_h330_face";
+      "https://image.tmdb.org/t/p/w116_and_h174_face";
 
   static const String upcomingMovies = "/movie/upcoming";
   static const String searchMovies = "/search/movie";
+  static const String movieGenres = "/genre/movie/list";
 }
