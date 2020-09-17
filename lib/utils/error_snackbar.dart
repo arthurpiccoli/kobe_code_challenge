fb1ff8284b28c3aac7d9eb73b8749438a83d6c2e
Arthur Piccoli <arthurpiccoli@outlook.com>
2020-09-17 11:34:16 -0300

    change error snackbar style

--- lib/utils/error_snackbar.dart
+++ lib/utils/error_snackbar.dart
@@ -4,9 +4,7 @@ import 'package:get/get.dart';
 void showErrorSnackbar(dynamic error) => Get.snackbar(
       "Oops, an error has ocurred!",
       error.toString(),
-      snackPosition: SnackPosition.BOTTOM,
       colorText: Colors.black,
-      backgroundColor: Colors.white,
-      duration: Duration(seconds: 5),
+      backgroundColor: Get.theme.accentColor,
       margin: EdgeInsets.all(10),
     );
