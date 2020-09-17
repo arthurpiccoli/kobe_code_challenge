e0a9021800e3bedefb152c61a02a2ccc72e5e3c4
Arthur Piccoli <arthurpiccoli@outlook.com>
2020-09-17 14:01:31 -0300

    create movie details list view

--- lib/views/movie_details/movie_details_page.dart
+++ lib/views/movie_details/movie_details_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kobe_code_challenge/constants/endpoints.dart';
 import 'package:kobe_code_challenge/models/movie.dart';
 import 'package:kobe_code_challenge/views/movie_details/widgets/movie_details_app_bar.dart';
-import 'package:kobe_code_challenge/utils/date_formatter.dart';
+
+import 'package:kobe_code_challenge/views/movie_details/widgets/movie_details_list_view.dart';
 
 class MovieDetailsPage extends StatelessWidget {
   final Movie movie = Get.arguments;
@@ -15,45 +15,7 @@ class MovieDetailsPage extends StatelessWidget {
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MovieDetailsAppBar(movie),
         ],
-        body: ListView(
-          children: [
-            _buildInfoRow(),
-            Divider(),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(movie.overview),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _buildInfoRow() {
-    return Padding(
-      padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
-      child: Row(
-        children: [
-          if (movie.posterPath != null)
-            Padding(
-              padding: EdgeInsets.only(right: 20),
-              child: Image.network(
-                Endpoints.posterUrl + movie.posterPath,
-                width: 120,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-          Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Release date:\n${movie.releaseDate.toShortDateString()}"),
-                Divider(),
-                Text("Genres:\n${movie.genreIds}")
-              ],
-            ),
-          ),
-        ],
+        body: MovieDetailsListView(movie),
       ),
     );
   }
---
+++ lib/views/movie_details/widgets/movie_details_list_view.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kobe_code_challenge/constants/endpoints.dart';
+import 'package:kobe_code_challenge/controllers/genres_controller.dart';
+import 'package:kobe_code_challenge/models/movie.dart';
+import 'package:kobe_code_challenge/utils/date_formatter.dart';
+
+class MovieDetailsListView extends StatelessWidget {
+  final Movie movie;
+  MovieDetailsListView(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: Row(
+            children: [
+              if (movie.posterPath != null) _buildPoster(),
+              _buildInfoColumn(),
+            ],
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(movie.overview),
+        ),
+      ],
+    );
+  }
+
+  _buildPoster() {
+    return Padding(
+      padding: EdgeInsets.only(right: 20),
+      child: Image.network(
+        Endpoints.posterUrl + movie.posterPath,
+        width: 116,
+        height: 174,
+      ),
+    );
+  }
+
+  _buildInfoColumn() {
+    return Flexible(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildTitleText("Release date"),
+          _buildInfoText("${movie.releaseDate.toShortDateString()}"),
+          Divider(),
+          _buildTitleText("Genres"),
+          _buildInfoText("${_formatGenreNames()}")
+        ],
+      ),
+    );
+  }
+
+  _buildTitleText(String text) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 5),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white60),
+      ),
+    );
+  }
+
+  _buildInfoText(String text) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 15),
+    );
+  }
+
+  _formatGenreNames() {
+    List<String> genreNames = Get.find<GenresController>()
+        .mapMovieGenreIdsToGenreNames(movie.genreIds);
+    return genreNames.reduce((value, element) => value + '\n' + element);
+  }
+}
