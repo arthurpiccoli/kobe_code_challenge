b61443416058516eee3ae04e405b15f02f4e94e8
Arthur Piccoli <arthurpiccoli@outlook.com>
2020-09-17 13:59:39 -0300

    inject genres controller in memory

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kobe_code_challenge/controllers/genres_controller.dart';
 import 'package:kobe_code_challenge/controllers/upcoming_movies_controller.dart';
 import 'package:kobe_code_challenge/views/widgets/movie_cards_list_view.dart';
 import 'package:kobe_code_challenge/views/widgets/rounded_flat_button.dart';
 
 class HomePage extends StatelessWidget {
   final upcomingMoviesController = Get.put(UpcomingMoviesController());
+  final genresController = Get.put(GenresController());
 
   @override
   Widget build(BuildContext context) {
